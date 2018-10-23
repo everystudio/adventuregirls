e3d711538c0258801733021405538c96ea616b6a
watanabe@tiffa <watanabe.everystudio@gmail.com>
2018-10-24 00:08:56 +0900

    スクリプトやらを追加

---
+++ Assets/10script/CharaIcon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharaIcon : MonoBehaviour {
+
+	public Button m_btn;
+	public Image m_imgIcon;
+
+	public EnergyBar m_barHP;
+	public EnergyBar m_barMP;
+
+}
